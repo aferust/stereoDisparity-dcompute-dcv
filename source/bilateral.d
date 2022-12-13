496b70ecc9f90bc71c349200528d6f10e0dceb1e
aferust <aferust@gmail.com>
2022-12-13 16:26:13 +0300

    minor edit

--- source/bilateral.d
+++ source/bilateral.d
@@ -7,6 +7,7 @@ import dcompute.std.index;
 import dcompute.std.cuda.sync;
 
 import dcompute.std.memory;
+import dcompute.std.cuda.math : ex2_approx_f, saturate_f;
 
 // https://github.com/NVIDIA/cuda-samples/blob/master/Samples/5_Domain_Specific/bilateralFilter/bilateral_kernel.cu
 
@@ -55,20 +56,13 @@ float __expf(float x){ // intrinsic?
 
     return sum;
 }
-*/
 
-pragma(LDC_intrinsic, "llvm.nvvm.ex2.approx.f")
-float __expf(float val);
-
-/*
 float saturatef(float val){ // intrinsic?
     if(val <= 0) return 0;
     if(val >= 1) return 1;
     return val;
 }
 */
-pragma(LDC_intrinsic, "llvm.nvvm.saturate.f")
-float saturatef(float val);
 
 float euclideanLen(float4 a, float4 b, float d)
 {
@@ -77,15 +71,15 @@ float euclideanLen(float4 a, float4 b, float d)
                 (b.y - a.y) * (b.y - a.y) +
                 (b.z - a.z) * (b.z - a.z);
 
-    return __expf(-mod / (2.0f * d * d));
+    return ex2_approx_f(-mod / (2.0f * d * d));
 }
 
 uint rgbaFloatToInt(float4 rgba)
 {
-    rgba.x = saturatef(abs(rgba.x));   // clamp to [0.0, 1.0]
-    rgba.y = saturatef(abs(rgba.y));
-    rgba.z = saturatef(abs(rgba.z));
-    rgba.w = saturatef(abs(rgba.w));
+    rgba.x = saturate_f(abs(rgba.x));   // clamp to [0.0, 1.0]
+    rgba.y = saturate_f(abs(rgba.y));
+    rgba.z = saturate_f(abs(rgba.z));
+    rgba.w = saturate_f(abs(rgba.w));
     return (cast(uint)(rgba.w * 255.0f) << 24) | (cast(uint)(rgba.z * 255.0f) << 16) | (cast(uint)(rgba.y * 255.0f) << 8) | cast(uint)(rgba.x * 255.0f);
 }
 
@@ -127,7 +121,7 @@ uint[3] blockSize = [16, 16, 1];
     for (int i = 0; i < 2*radius + 1; ++i)
     {
         float _x = i-radius;
-        cGaussian[i] = __expf(-(_x*_x) / (2*delta*delta));
+        cGaussian[i] = ex2_approx_f(-(_x*_x) / (2*delta*delta));
     }
 
     float sum = 0.0f;
